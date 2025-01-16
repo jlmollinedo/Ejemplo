a031f3f0b1ea8667c21756903d3059260798ae7e
jlmollinedo <118819197+jlmollinedo@users.noreply.github.com>
2025-01-16 11:07:54 +0100

    Add files via upload

---
+++ Tutorial.java
@@ -0,0 +1,7 @@
+//ejemplo de programa en java
+//Esto es una prueba para ver los cambios
+public class Tutorial{
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
